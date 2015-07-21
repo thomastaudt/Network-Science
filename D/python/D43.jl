bb2373219de1a9eb4073ce87f629445dc9bd59ee
Thomas Staudt <ts.staudt@yahoo.de>
2015-07-21 16:04:41 +0200

    everything works perfectly well

--- D/python/D43.jl
+++ D/python/D43.jl
@@ -2,12 +2,14 @@
 """
 Created on Mon Jul 20 12:58:18 2015
 
-@author: Erik
+@author: Erik, Thomas
 """
 @time begin
-edges = readdlm("data/web-Stanford.txt", Int32)
-
-mat = sparse( edges[:, 1], edges[:, 2], ones(size(edges)[1]) )
+edges = readdlm(ARGS[1], Int32) + 1
+println(size(edges))
+println(size(edges[:, 1]), size(edges[:, 2]))
+mat = sparse( edges[:, 1], edges[:, 2], ones(size(edges)[1]), maximum(edges), maximum(edges), (x,y) -> 1 )
+println(size(mat))
 rsums = sum(mat, 2)
 mat = spdiagm(1.0./rsums[:, 1]) * mat
 
@@ -19,12 +21,13 @@ v = ones(length(rsums)) / length(rsums)
 # DO NOT SHOW  MAT!!!
 x0 = deepcopy(v)
 
+mat = mat'
 
 function update(xk::Array{Float64}, mat::SparseMatrixCSC, a::Vector{Float64}, v::Vector{Float64}, α::Float64)
   xn = α * mat * xk + (α * xk⋅a + (1-α)) * v
 end
 
-for i = 1:25
+for i = 1:20
   x0 = update(x0, mat, a, v, 0.85)
 end
 
