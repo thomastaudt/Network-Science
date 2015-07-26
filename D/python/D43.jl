52b08b06ba0724f4a2fdd6ac0b0509df954c1746
Thomas Staudt <ts.staudt@yahoo.de>
2015-07-26 13:07:58 +0200

    improved D43.jl

--- D/python/D43.jl
+++ D/python/D43.jl
@@ -1,41 +1,45 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 20 12:58:18 2015
 
-@author: Erik, Thomas
-"""
 @time begin
-edges = readdlm(ARGS[1], Int32) + 1
-println(size(edges))
-println(size(edges[:, 1]), size(edges[:, 2]))
-mat = sparse( edges[:, 1], edges[:, 2], ones(size(edges)[1]), maximum(edges), maximum(edges), (x,y) -> 1 )
-println(size(mat))
+@assert length(ARGS) == 1 "No edge file to load given"
+edges = readdlm(ARGS[1], Int32) 
+println("Read edge file with $(size(edges)[1]) edges.")
+edges += 1 - minimum(edges)
+mat = sparse( edges[:,1], edges[:,2], ones(size(edges)[1]), 
+              maximum(edges), maximum(edges), (x,y) -> 1 )
+
+# construct the matrix
+println("Sparse matrix of size $(size(mat)) constructed")
 rsums = sum(mat, 2)
 mat = spdiagm(1.0./rsums[:, 1]) * mat
-
+mat = mat'
 
 # dangling nodes
 a = [(i == 0 ? 1.0 : 0.0) for i in rsums]
 v = ones(length(rsums)) / length(rsums)
 
-# DO NOT SHOW  MAT!!!
+# need to copy this 
 x0 = deepcopy(v)
 
-mat = mat'
 
 function update(xk::Array{Float64}, mat::SparseMatrixCSC, a::Vector{Float64}, v::Vector{Float64}, α::Float64)
   xn = α * mat * xk + (α * xk⋅a + (1-α)) * v
 end
 
+println("Start ranking iterations")
+
 for i = 1:20
+  println(i)
   x0 = update(x0, mat, a, v, 0.85)
 end
 
 # to get nicer values
 score = x0 - mean(x0)
 score /= maximum(score)
-
 order = sortperm(score, rev=true)
+
+# print the final result
+println("Obtained Ranks")
 for i = 1:10
   println(order[i], " ", score[order[i]])
 end
